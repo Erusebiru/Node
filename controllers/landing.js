f8f58ad0d326ec3c4341f4dc0a372b8e636341f8
Erusebiru <Erusebiru@gmail.com>
2019-04-16 20:42:55 +0200

    form insert in database

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,10 +1,14 @@
 
+const models = require('../models');
 
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Express' });
 }
 
 exports.submit_lead = function(req,res,next){
-    console.log("Nombre: ", req.body.nombre);
-    res.redirect('/');
+    return models.User.create({
+        nombre: req.body.nombre
+    }).then(user => {
+        res.redirect('/');
+    });
 }
