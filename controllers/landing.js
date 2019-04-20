bc82d0f90981383d28eb77955d9729b9b3eea4ad
Erusebiru <Erusebiru@gmail.com>
2019-04-20 13:17:08 +0200

    User details

--- controllers/landing.js
+++ controllers/landing.js
@@ -9,6 +9,22 @@ exports.submit_lead = function(req,res,next){
     return models.User.create({
         nombre: req.body.nombre
     }).then(user => {
-        res.redirect('/');
+        res.redirect('/users');
     });
+}
+
+exports.show_users = function(req,res,next){
+    models.User.findAll().then(users => {
+        res.render('landing', {title:'Express',users: users});
+    })
+}
+
+exports.show_user = function(req,res,next){
+    return models.User.findOne({
+        where: {
+            id: req.params.user_id
+        }
+    }).then(user => {
+        res.render('user', { user: user });
+    })
 }
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const landing = require('../controllers/landing');
-/* GET home page. */
+
 router.get('/', landing.get_landing);
 
 router.post('/',landing.submit_lead);
+
+router.get('/users', landing.show_users);
+
+router.get('/user/:user_id',landing.show_user);
 
 module.exports = router;
